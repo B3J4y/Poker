34c7930615651f69b30e1a3a4e8fe7e054e1d1c2
B3J4y <janbernoth@gmail.com>
2014-01-01 15:02:25 +0100

    paar Dateien vergessen

---
+++ SurfacePoker/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SurfacePoker
+{
+    public class Action
+    {
+        public Action(playerAction pa, int amount)
+        {
+            action = pa;
+            this.amount = amount;
+        }
+
+        public Action()
+        {
+
+        }
+        public enum playerAction
+        {
+            bet, raise, call, check, fold
+        }
+        public int amount { get; set; }
+        public playerAction action { get; set; }
+    }
+}
---
+++ SurfacePoker/Pot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SurfacePoker
+{
+
+public class Pot
+    {
+        public int value { get; set; }
+        public int raiseSize { get; set; }
+        public int amountPerPlayer { get; set; }
+        public List<Pot> sidePots { get; set; }
+	    public Pot()
+	    {
+            value = 0;
+            sidePots = new List<Pot>();
+	    }
+
+        public void raisePot(int value)
+        {
+            this.value += value;
+            //Todo: Sidepots
+        }
+    }
+}
